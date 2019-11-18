9a55e6775d4caa94dd0022f52a495c1540ec5d38
Beth Osyk <bosyk@ecr.ai>
2019-11-18 10:46:24 -0500

    Removed default braking behavior when too close in order to use RSS-defined behavior instead

--- code/scenario_runner_extension/rss_basic_agent.py
+++ code/scenario_runner_extension/rss_basic_agent.py
@@ -5,7 +5,7 @@ from scenario_runner_extension.rss_aux import RssSensor
 
 class RssBasicAgent(BasicAgent):
 	def __init__(self, vehicle, target_speed, rss_params):
-		
+
 		super(RssBasicAgent, self).__init__(vehicle, target_speed)
 
 		self._rss_sensor = RssSensor(vehicle, rss_params)
@@ -21,12 +21,16 @@ class RssBasicAgent(BasicAgent):
 			rss_restriction = self._rss_sensor.acceleration_restriction if self._rss_sensor and self._rss_sensor.response_valid else None
 			if rss_restriction:
 				#carla.VehiclePhysicsControl()
-				vehicle_control_rss = self._restrictor.restrictVehicleControl(control, 
-																			  rss_restriction, 
-																			  self._rss_sensor.ego_velocity, 
+                                #vehicle control rss
+				#VehicleControl(throttle=1.000000, steer=-1.000000, brake=0.000000, hand_brake=False, reverse=False, manual_gear_shift=False, gear=0)
+
+				vehicle_control_rss = self._restrictor.restrictVehicleControl(control,
+																			  rss_restriction,
+																			  self._rss_sensor.ego_velocity,
 																			  self._physics_control_static)
-				#if not (control == vehicle_control_rss):
-				#	print('RSS restrictor is ON: brake=%.3f, steer=%.3f' % (vehicle_control_rss.brake, vehicle_control_rss.steer))
+																			  
+				if not (control == vehicle_control_rss):
+					print('RSS restrictor is ON: brake=%.3f, steer=%.3f' % (vehicle_control_rss.brake, vehicle_control_rss.steer))
 				control = vehicle_control_rss
 		#-------------------------------------
 		return control
