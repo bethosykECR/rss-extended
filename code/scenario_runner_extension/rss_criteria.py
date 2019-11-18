fa237c173a37337a5e6e2d7c86c2e6c533c80427
Beth Osyk <bosyk@ecr.ai>
2019-11-18 10:45:24 -0500

    Edited code to account for scenarios where only the ego vehicle is present.  Code previously presumed at least one additional vehicle.

--- code/scenario_runner_extension/rss_criteria.py
+++ code/scenario_runner_extension/rss_criteria.py
@@ -18,26 +18,27 @@ class RssTest(Criterion):
         # get distance between BBX
         d = dist_aux.evaluate_dist(self.vehicles)
 
-        # get ego velocity 
+        # get ego velocity
         ego_v = self.actor.get_velocity()
         ego_velocity = math.sqrt(ego_v.x**2 + ego_v.y**2 + ego_v.z**2) # [m/s]
-          
-        pov = [actor for actor in self.vehicles if actor.attributes.get('role_name') != 'hero'][0]
-        pov_v = pov.get_velocity()
-        pov_velocity = math.sqrt(pov_v.x**2 + pov_v.y**2 + pov_v.z**2) # [m/s]
-        #print('EGO_V = %s, POV_V = %s' % (ego_velocity, pov_velocity))
-        #pos_ego = self.actor.get_location()
-        #pos_pov = pov.get_location()
-        #print('pos_ego = (%.6f, %.6f, %.6f), pos_pov = (%.6f, %.6f, %.6f)' % (pos_ego.x, pos_ego.y, pos_ego.z, pos_pov.x, pos_pov.y, pos_pov.z)) 
-        other_aux.write2csv(self.filename, [d, ego_velocity, pov_velocity])
+
+        if (len(self.vehicles) > 1):
+            pov = [actor for actor in self.vehicles if actor.attributes.get('role_name') != 'hero'][0]
+            pov_v = pov.get_velocity()
+            pov_velocity = math.sqrt(pov_v.x**2 + pov_v.y**2 + pov_v.z**2) # [m/s]
+            #print('EGO_V = %s, POV_V = %s' % (ego_velocity, pov_velocity))
+            #pos_ego = self.actor.get_location()
+            #pos_pov = pov.get_location()
+            #print('pos_ego = (%.6f, %.6f, %.6f), pos_pov = (%.6f, %.6f, %.6f)' % (pos_ego.x, pos_ego.y, pos_ego.z, pos_pov.x, pos_pov.y, pos_pov.z))
+            #other_aux.write2csv(self.filename, [d, ego_velocity, pov_velocity])
         return py_trees.common.Status.RUNNING
 
 
         #self.rss_params = rss_params
         #self.rss_dynamics = RssSensor(self.actor, self.rss_params).sensor.ego_vehicle_dynamics
         '''
-        d_rss = max(0, 
-                ego_velocity * self.rss_dynamics.response_time.value + 
+        d_rss = max(0,
+                ego_velocity * self.rss_dynamics.response_time.value +
                 1/2*self.rss_dynamics.alpha_lon.accel_max.value * self.rss_dynamics.response_time.value** +
                 1/(2*self.rss_dynamics.alpha_lon.brake_min.value)*
                     (ego_velocity + self.rss_dynamics.alpha_lon.accel_max.value*self.rss_dynamics.response_time.value)** -
