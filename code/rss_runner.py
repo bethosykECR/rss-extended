ddc5a8a5b072b1b76bb610ef8efc30f072169cf5
Beth Osyk <bosyk@ecr.ai>
2019-11-18 10:43:36 -0500

    Changes to runner to remove file write and to accept a scenario name

--- code/rss_runner.py
+++ code/rss_runner.py
@@ -19,19 +19,23 @@ from tools import robustness
 
 from scenario_runner_extension.rss_aux import defineRssParams
 from scenario_runner_extension.rss_aux import RssParamsInit
-from scenario_runner_extension.rss_config_parser import parse_rss_scenario_configuration 
+from scenario_runner_extension.rss_config_parser import parse_rss_scenario_configuration
 
-from scenarios.rss_opposite_vehicle_taking_priority import RssOppositeVehicleRunningRedLight
-from scenarios.rss_lvdad import RssLVDAD
-from scenarios.rss_follow_leading_vehicle import RssFollowLeadingVehicle
-from scenarios.rss_pov_unprotected_left import RssPovUnprotectedLeft
+from scenarios.rss_ext_forward_incursion import RssExtForwardIncursion
+from scenarios.rss_ext_side_incursion import RssExtSideIncursion
+from scenarios.rss_ext_shorter_following_distance import RssExtShorterFollowingDistance
+from scenarios.rss_ext_platooning import RssExtPlatooning
+from scenarios.rss_ext_merge import RssExtMerge
+from scenarios.rss_ext_shorter_following_distance_multiple_geometry import RssExtShorterFollowingDistanceMG
+from scenarios.rss_ext_occlusion import RssExtOcclusion
+from scenarios.rss_ext_temp_occlusion import RssExtTempOcclusion
+from scenarios.rss_ext_pass_pedestrian import RssExtPassPedestrian
+from scenarios.rss_ext_parking_lot import RssExtParkingLot
 
-RES_FOLDER = '../results-' + time.strftime("%d-%H-%M-%S")
-if not os.path.exists(RES_FOLDER):
-    os.makedirs(RES_FOLDER)
-TRAJ_FILENAME = os.path.join(RES_FOLDER, 'trajectory.csv')
-
-
+#RES_FOLDER = '../results-' + time.strftime("%d-%H-%M-%S")
+#if not os.path.exists(RES_FOLDER):
+#    os.makedirs(RES_FOLDER)
+#TRAJ_FILENAME = os.path.join(RES_FOLDER, 'trajectory.csv')
 
 class ScenarioRunner(object):
 
@@ -48,8 +52,8 @@ class ScenarioRunner(object):
     ego_vehicles = []
 
     # Tunable parameters
-    client_timeout = 30.0  # in seconds
-    wait_for_world = 20.0  # in seconds
+    client_timeout = 60.0  # in seconds
+    ego_drive_world = 20.0  # in seconds
     world = None
     manager = None
     additional_scenario_module = None
@@ -66,18 +70,6 @@ class ScenarioRunner(object):
         # requests in the localhost at port 2000.
         self.client = carla.Client(args.host, int(args.port))
         self.client.set_timeout(self.client_timeout)
-
-        # Once we have a client we can retrieve the world that is currently
-        # running.
-        self.world = self.client.get_world()
-
-        settings = self.world.get_settings()
-        #settings.synchronous_mode = True
-        self.world.apply_settings(settings)
-
-        CarlaActorPool.set_world(self.world)
-        CarlaDataProvider.set_world(self.world)
-
 
     def __del__(self):
         """
@@ -103,9 +95,12 @@ class ScenarioRunner(object):
         """
         Remove and destroy all actors
         """
-        settings = self.world.get_settings()
-        settings.synchronous_mode = False
-        self.world.apply_settings(settings)
+        # TODO:  Print this to a file instead
+        # Scenario name, start time, end time, stats
+        print('Start time')
+        print(self._start_time)
+        print('End time')
+        print(self.world.tick())
 
         CarlaDataProvider.cleanup()
         CarlaActorPool.cleanup()
@@ -156,8 +151,7 @@ class ScenarioRunner(object):
             for i, _ in enumerate(self.ego_vehicles):
                 self.ego_vehicles[i].set_transform(config.ego_vehicles[i].transform)
 
-        # sync state
-        CarlaDataProvider.get_world().tick()
+        self._start_time = CarlaDataProvider.get_world().tick()
 
     def analyze_scenario(self, args, config):
         """
@@ -180,25 +174,19 @@ class ScenarioRunner(object):
         else:
             print("Failure!")
 
-    def load_world(self, args, town):
+    def load_world(self, town):
         """
         Load a new CARLA world and provide data to CarlaActorPool and CarlaDataProvider
         """
+        self.world = self.client.load_world(town)
 
-        if args.reloadWorld:
-            self.world = self.client.load_world(town)
-        else:
-            if CarlaDataProvider.get_map().name != town:
-                print("The CARLA server uses the wrong map!")
-                print("This scenario requires to use map {}".format(town))
-                return False
+        # Wait for the world to be ready
+        self.world.tick()
 
         CarlaActorPool.set_client(self.client)
         CarlaActorPool.set_world(self.world)
         CarlaDataProvider.set_world(self.world)
 
-        # Wait for the world to be ready
-        self.world.tick()
         return True
 
     def load_and_run_scenario(self, args, config, scenario):
@@ -211,7 +199,8 @@ class ScenarioRunner(object):
         self.manager.run_scenario()
 
         # Provide outputs if required
-        self.analyze_scenario(args, config)
+        # TODO:  Add new metrics
+        # self.analyze_scenario(args, config)
 
         # Stop scenario and cleanup
         self.manager.stop_scenario()
@@ -221,19 +210,20 @@ class ScenarioRunner(object):
 
 
     def simulate(self, config, args, rss_params):
-        file = open(self.filename_traj, 'w')  
-        file.close()
+        #file = open(self.filename_traj, 'w')
+        #file.close()
         result = False
         scenario_class = self.get_scenario_class_or_fail(config.type)
 
         while not result:
             try:
-                self.load_world(args, config.town)
-                self.manager = ScenarioManager(self.world, args.debug)   
-                CarlaActorPool.set_world(self.world)
+                #moved to init() to load world only once
+                #self.load_world(args, config.town)
+                self.manager = ScenarioManager(self.world, args.debug)
+                #CarlaActorPool.set_world(self.world)
                 self.prepare_ego_vehicles(config)
-                self.prepare_camera(config)    
-                scenario = scenario_class(self.world, rss_params, self.filename_traj, self.ego_vehicles, config, args.randomize, args.debug)
+                self.prepare_camera(config)
+                scenario = scenario_class(self.world, rss_params, self.filename_traj, self.ego_vehicles, config, args.randomize, args.debug, variant=ARGUMENTS.scenario)
                 result = True
             except Exception as exception:
                 print("The scenario cannot be loaded")
@@ -242,55 +232,51 @@ class ScenarioRunner(object):
                 self.cleanup()
                 pass
         self.load_and_run_scenario(args, config, scenario)
-        rob = robustness.getRobustness(self.filename_traj)
+        #rob = robustness.getRobustness(self.filename_traj)
 
-        return rob
+        #return rob
+        return 0
 
+    def run(self, args):
+        scenario_config_file = ScenarioConfigurationParser.find_scenario_config(args.scenario, args.configFile) # xml file
 
-    def run(self, args): 
-        scenario_config_file = ScenarioConfigurationParser.find_scenario_config(args.scenario, args.configFile) # xml file 
-
-        num_simult_runs = 1
-        nruns = 3000
         ####################################
+        print(args.configFile)
+        print(scenario_config_file)
+        print(args.scenario)
+        scenario_configurations = parse_rss_scenario_configuration(scenario_config_file, args.scenario)
+        config = scenario_configurations[0] # since we work only with one scenario!
 
-        search_names = ['alpha_lon_accel_max', 'response_time']
-        alpha_lon_accel_max = 3.5
-        response_time = 1.0
-        ####################################
-        x0, searchSpace = RssParamsInit().getInit(search_names,
-                                                   alpha_lon_accel_max = alpha_lon_accel_max,
-                                                   response_time = response_time)
-        print('X0 = %s' % x0)
-        print('SearchSpace = %s\n' % searchSpace)
-        #-------------------------------
-        X0 =[]
-        for _ in range(num_simult_runs):
-            X0.append(x0)
-        ####################################
+	# Defaults except for ones we set
+        # Lon accel max: 3.500
+        # Lon brake max: 8.000
+        # Lon brake min: 4.000
+        # Lon brake min correct: 0.100
+        # Lat accel max: 0.200
+        # Lat brake min: 0.800
+        # Lat fluct mar: 0.000
+        # Response time: 0.200
 
+        g = 9.8
+        rss_params = {}
+        rss_params['alpha_lon_accel_max'] = 0.5*g
+        rss_params['alpha_lon_brake_max'] = 0.7*g
+        rss_params['response_time'] = 0.2
+        rss_params['alpha_lon_brake_min'] = 0.5*g
+        print('RSS params: %s' % rss_params)
 
-        ####################################        
-        def ff(x):
-            scenario_configurations = parse_rss_scenario_configuration(scenario_config_file, args.scenario)
-            config = scenario_configurations[0] # since we work only with one scenario!
+        # Load the world once
+        self.load_world(config.town)
 
-            rss_params = defineRssParams(x, search_names)
-            rss_params['alpha_lon_brake_min_correct'] = 0.1
-            print('RSS params: %s' % rss_params)
+        settings = self.world.get_settings()
+        settings.fixed_delta_seconds = 0.015
+        self.world.apply_settings(settings)
 
-            return self.simulate(config, args, rss_params)
+        # Set the sun to be directly overhead
+        weather = carla.WeatherParameters(sun_altitude_angle=90)
+        self.world.set_weather(weather)
 
-        # reproduce trajs
-        '''
-        for i in range(10):
-            self.filename_traj = os.path.join(RES_FOLDER, ('trajectory'+str(i)+'.csv'))
-            ff(x0)
-        '''
-        best_x_history, best_f_history, x_history, f_history, accept_x_history, accept_flags = annealing.runFunc(ff, X0, searchSpace, nruns, num_simult_runs, RES_FOLDER) 
-
-
-
+        return self.simulate(config, args, rss_params)
 
 if __name__ == '__main__':
 
@@ -300,52 +286,130 @@ if __name__ == '__main__':
     PARSER.add_argument('--host', default='127.0.0.1', help='IP of the host server (default: localhost)')
     PARSER.add_argument('--port', default='2000', help='TCP port to listen to (default: 2000)')
     PARSER.add_argument('--debug', action="store_true", help='Run with debug output')
-    PARSER.add_argument('--output', action="store_true", help='Provide results on stdout')
-    PARSER.add_argument('--file', action="store_true", help='Write results into a txt file')
-    PARSER.add_argument('--junit', action="store_true", help='Write results into a junit file')
-    PARSER.add_argument('--outputDir', default='', help='Directory for output files (default: this directory)')
-    PARSER.add_argument('--waitForEgo', action="store_true", help='Connect the scenario to an existing ego vehicle')
-    PARSER.add_argument('--configFile', default='', help='Provide an additional scenario configuration file (*.xml)')
+    PARSER.add_argument('--scenario', default='Rss_Ext_FI_a', help='Name of the scenario to run')
+    #PARSER.add_argument('--output', action="store_true", help='Provide results on stdout')
+    #PARSER.add_argument('--file', action="store_true", help='Write results into a txt file')
+    #PARSER.add_argument('--junit', action="store_true", help='Write results into a junit file')
+    #PARSER.add_argument('--outputDir', default='', help='Directory for output files (default: this directory)')
+    #PARSER.add_argument('--waitForEgo', action="store_true", help='Connect the scenario to an existing ego vehicle')
+    #PARSER.add_argument('--configFile', default='', help='Provide an additional scenario configuration file (*.xml)')
     PARSER.add_argument('--randomize', action="store_true", help='Scenario parameters are randomized')
     ARGUMENTS = PARSER.parse_args()
     ARGUMENTS.reloadWorld = True
-    ARGUMENTS.filename_traj = TRAJ_FILENAME
+    #ARGUMENTS.filename_traj = TRAJ_FILENAME
+    ARGUMENTS.filename_traj = ""
     ARGUMENTS.configFile = os.path.join(os.getcwd(), 'rss.xml') # do not change this line
     ###############################################################
     # CHOOSE THE SCENARIO:
     ###############################################################
-    # 1. Rss_LVS: Leading Vehicle Stopped 
-    #    (One of the Automatic Emergency breaking scenarios (AEB))
-    #       EV velocity:  40.2 km/h = 11.17 m/s
-    #       POV velocity: 0 km/h    = 0 m/s
-    # 2. Rss_LVM1: Leading Vehicle Moving, scenario #1 
-    #    (One of the Automatic Emergency breaking scenarios (AEB))
-    #       EV velocity:  40.2 km/h = 11.17 m/s
-    #       POV velocity: 16.1 km/h = 4.47 m/s
-    # 3. Rss_LVM2: Leading Vehicle Moving, scenario #2 
-    #    (One of the Automatic Emergency breaking scenarios (AEB))
-    #       EV velocity:  72.4 km/h = 20.1 m/s
-    #       POV velocity: 32.2 km/h = 8.9 m/s
-    # 4. Rss_LVD: Leading Vehicle Decelerating
-    #    (One of the Automatic Emergency breaking scenarios (AEB))
-    #       EV velocity:  56.3 km/h = 15.6 m/s
-    #       POV velocity: 56.3 km/h = 15.6 m/s
-    # 5. Rss_LVDAD: Leading Vehicle Decelerates, Accelerates, then Decelerates
-    #    (Traffic Jam Assist scenarios (TJA))
-    #       EV velocity:  40.2 km/h = 11.17 m/s
-    #       POV velocity: 40.2 km/h = 11.17 m/s
-    # 6. Rss_OppositeVehicleRunningRedLight
-    #    (Intersection scenarios)
-    # 7. Rss_PovUnprotectedLeft
-    #    (Intersection scenarios)
+    # 1. Rss_Ext_FI:  Forward incursion
+    #    Other vehicle cuts in between ego vehicle and lead vehicle.
+    #       Rss_Ext_FI_a:  RSS Classic max braking response
+    #       Rss_Ext_FI_b:  RSS Extended gentle braking response
+    #
+    # 2. Rss_Ext_SI:  Side incursion
+    #    Other vehicle intrudes partially upon lane of ego vehicle
+    #       Rss_Ext_SI_a:  Small intrusion.  RSS Classic ego vehicle stops.
+    #       Rss_Ext_SI_b:  Small intrusion.  RSS Extended ego vehicle continues in-lane.
+    #       Rss_Ext_SI_c:  Large intrusion.  RSS Extended ego vehicle changes lanes.
+    #
+    # 3. Rss_Ext_SFD:  Shorter following distance
+    #     By setting limits on its own acceleration and braking, ego chooses a
+    #     shorter following distance.
+    #       Rss_Ext_SFD_a:  RSS Classic following distance
+    #       Rss_Ext_SFD_b:  RSS Extended shorter following distance
+    #
+    # 4. Rss_PLAT:  Platooning
+    #     Leveraging vehicle-to-vehicle communication could allow a vehicle
+    #     to obtain a short response time and to coordinate acceleration.
+    #     Similar implementation to Shorter Following Distance.
+    #       Rss_PLAT_a:  RSS Classic following distance
+    #       Rss_PLAT_b:  RSS Extended shorter following distance
+    #
+    #  5. Rss_Ext_MERGE:  Merge
+    #    By setting limits on its own acceleration and braking, ego can merge
+    #    into a tighter gap.
+    #      Rss_Ext_MERGE_a: Current RSS library does not warn on merge case and ego may
+    #         collide with follower vehicles.
+    #      Rss_Ext_MERGE_b: RSS Classic behavior prohibits too-short safe following distance
+    #         with new leader and ego cannot complete merge.
+    #      Rss_Ext_MERGE_c: RSS Extended allows for shorter safe following distance and the
+    #         ego vehicle is able to complete the merge.
+    #
+    #  6. Rss_Ext_SFDMG:  Shorter following distance multiple geometry
+    #     By setting limits on its own acceleration and braking, ego chooses a
+    #     shorter following distance entering a roundabout.
+    #      Rss_Ext_SFDMG_a: Current RSS library does not yet include roundabouts and ego
+    #         may collide with lead vehicle.
+    #      Rss_Ext_SFDMG_b: Ego vehicle enters roundabout at RSS Classic safe following
+    #         distance.
+    #      Rss_Ext_SFDMG_c: RSS Extended ego vehicle sets limits on its own acceleration
+    #         and braking to achieve a shorter following distance.
+    #
+    #  7. Rss_Ext_OCCL:  Occlusion
+    #     Ego vehicle must decide yield behavior with respect to a vehicle
+    #     traveling straight through a T intersection.  A tree partially
+    #     occludes the intersection.
+    #      Rss_Ext_OCCL_a: Current RSS library does not yet include T intersection checking
+    #         and ego may collide with through vehicle.
+    #      Rss_Ext_OCCL_b: RSS Classic assumes an incoming vehicle with the worst-case
+    #         velocity (section 3.9), thus increasing the effect of the
+    #         occlusion.
+    #      Rss_Ext_OCCL_c: RSS Extended could permit an assumption of a lower velocity than
+    #         worst-case.
+    #
+    #  8. Rss_Ext_TEMPOCCL:  Temporary occlusion of traffic signal
+    #     The view of a traffic light is obstructed by a tall lead vehicle.
+    #     The ego vehicle decides whether to proceed without visibility of the
+    #     traffic light, or to wait until lead vehicle moves and the traffic
+    #     light is visible.
+    #     Rss_Ext_TEMPOCCL_a:  Ego vehicle proceeds without clear line of sight
+    #     Rss_Ext_TEMPOCCL_b:  Ego vehicle waits for clear line of sight
+    #
+    #  9. Rss_Ext_PASSPED:  Passing a pedestrian
+    #     The ego vehicle chooses a behavior when a pedestrian crosses the
+    #     street.
+    #     Rss_Ext_PASSPED_a:  Current RSS library does not cover pedestrians and the ego
+    #         vehicle may collide with a pedestrian.  This corresponds to
+    #         section 3.8 example 2 where the vehicle has the right-of-way.
+    #     Rss_Ext_PASSPED_b:  The ego vehicle is required to yield to a pedestrian in a
+    #         residential setting, section 3.8 example 1.
+    #     Rss_Ext_PASSPED_c:  The ego vehicle is required to remain outisde of a pedestrian's
+    #         expected route when walking along a residential road, section
+    #         3.8 example 3.
+    #
+    # 10. Rss_Ext_PARKLOT:  Parking lot
+    #     The ego vehicle drives through and exits a parking lot.
+    #     Rss_Ext_PARKLOT_a:  The ego vehicle exits the lot.  Parking lots might not be
+    #         considered driveable space by the RSS library yet, as the
+    #         vehicle tries to exit immediately.  Also, the fence locations
+    #         are not yet known by the ego vehicle.
     ###############################################################
-    ARGUMENTS.scenario = 'Rss_LVS'
-    #ARGUMENTS.scenario = 'Rss_LVM1'
-    #ARGUMENTS.scenario = 'Rss_LVM2'
-    #ARGUMENTS.scenario = 'Rss_LVD'
-    #ARGUMENTS.scenario = 'Rss_LVDAD'
-    #ARGUMENTS.scenario = 'Rss_OppositeVehicleRunningRedLight'
-    #ARGUMENTS.scenario = 'Rss_PovUnprotectedLeft'
+    # Scenario name choices
+    #ARGUMENTS.scenario = 'Rss_Ext_FI_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_FI_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_SI_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_SI_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_SI_c'
+    #ARGUMENTS.scenario = 'Rss_Ext_SFD_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_SFD_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_PLAT_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_PLAT_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_MERGE_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_MERGE_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_MERGE_c'
+    #ARGUMENTS.scenario = 'Rss_Ext_SFDMG_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_SFDMG_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_SFDMG_c'
+    #ARGUMENTS.scenario = 'Rss_Ext_OCCL_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_OCCL_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_OCCL_c'
+    #ARGUMENTS.scenario = 'Rss_Ext_TEMPOCCL_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_TEMPOCCL_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_PASSPED_a'
+    #ARGUMENTS.scenario = 'Rss_Ext_PASSPED_b'
+    #ARGUMENTS.scenario = 'Rss_Ext_PASSPED_c'
+    #ARGUMENTS.scenario = 'Rss_Ext_PARKLOT_a'
     ###############################################################
 
     SCENARIORUNNER = None
